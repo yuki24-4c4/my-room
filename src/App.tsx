4ffd21d9aca962647e5fdbe878d2784029100010
yuki24-4c4 <yuki244c4@gmail.com>
2025-05-16 11:16:30 +0900

    デプロイ頑張ってる3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/my-room">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-room', // リポジトリ名を指定
+  base: '/my-room/', // リポジトリ名を指定
   plugins: [react()],
 });
